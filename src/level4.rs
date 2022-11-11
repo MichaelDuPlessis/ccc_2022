931b0b0f40a863c66e887ec97d43fd3491dd5fc5
Reinhardt_Laptop <u20461632@tuks.co.za>
2022-11-11 18:49:29 +0200

    fasjdflksa

--- src/level4.rs
+++ src/level4.rs
@@ -1,5 +1,7 @@
 const LEVEL: &str = "level4";
 
+use petgraph::{algo, prelude::*};
+
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub enum Entity {
     Coin,
@@ -8,12 +10,39 @@ pub enum Entity {
     Space,
 }
 
-struct Game {
+pub struct Game {
 
 }
 
 impl Game {
-    pub fn new(path: &str) -> Self {
+    pub fn new(file: &str) -> Self {
+        let input = std::fs::read_to_string(format!("./{}/{}.in", LEVEL, file)).unwrap();
+
+        let mut lines = input.lines();
+
+        Self {
+
+        }
+    }
+
+    pub fn calculate() {
+        let mut graph = DiGraph::<&str, i32>::new();
+        
+        let a = graph.add_node("a");
+        let b = graph.add_node("b");
+        let c = graph.add_node("c");
+        let d = graph.add_node("d");
+
+        graph.extend_with_edges(&[(a, b, 1), (b, c, 1), (c, d, 1), (a, b, 1), (b, d, 1)]);
+
+        let ways = algo::all_simple_paths::<Vec<_>, _>(&graph, a, d, 0, None).collect::<Vec<_>>();
+
+        for way in &ways {
+            println!("{:?}", way.clone());
+        }
+
+        println!("end");
+
         
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ mod level3;
 mod level4;
 
 fn main() {
-    for i in 1..8 {
-        level3::Game::new(&format!("level4_{}", i)).move_pac();
-    }
+    // for i in 1..8 {
+    //     level3::Game::new(&format!("level4_{}", i)).move_pac();
+    // }
 
-    level3::Game::new(&format!("level4_{}", "example")).move_pac();
+    // level3::Game::new(&format!("level4_{}", "example")).move_pac();
+
+    let entity = level4::Game::new(&format!("level4_{}", "example"));
+
+    level4::Game::calculate();
 }
