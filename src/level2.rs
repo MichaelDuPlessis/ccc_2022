f66edc6fe817943c3602448c22b1184b1e8615c6
Reinhardt_Laptop <u20461632@tuks.co.za>
2022-11-11 16:48:44 +0200

    Game new()

--- src/level2.rs
+++ src/level2.rs
@@ -10,21 +10,52 @@ struct Game {
 }
 
 pub fn count_coins_over(file: &str) {
-    let input = std::fs::read_to_string(format!("./level2/{}.in", file)).unwrap();
 
-    let lines = input.lines();
 
-    let mut array: Vec<Vec<char>> = Vec::new();
 
-    for line in lines {
-        let new_row = Vec::new();
+}
 
-        for character in line.chars() {
-            new_row.push(character);
+impl Game {
+    pub fn new(file: &str) -> Self {
+        let input = std::fs::read_to_string(format!("./level2/{}.in", file)).unwrap();
+
+        let mut lines = input.lines();
+        
+        let num: usize = lines.next().unwrap().to_string().parse().unwrap();
+
+        let mut array: Vec<Vec<char>> = Vec::new();
+        
+        let mut coords: Coords = (0, 0);
+
+        let mut _movements = String::new();
+
+        for (index, line) in lines.enumerate() {
+            if index < 10 {
+                let mut new_row = Vec::new();
+    
+                for character in line.chars() {
+                    new_row.push(character);
+                }
+    
+                array.push(new_row);
+            }
+            else if index == 10 {
+                let mut indeces = line.split_whitespace();
+
+                coords.0 = indeces.next().unwrap().to_string().parse().unwrap();
+                coords.1 = indeces.next().unwrap().to_string().parse().unwrap();
+            }
+            else {
+                _movements = line.trim().to_string();
+            }
+
+        } 
+
+        Self {
+            size: num,
+            board: array,
+            movements: _movements,
+            pacman: coords,
         }
-
-        array.push(new_row);
     }
-
-
 }
