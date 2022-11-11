4a7da3cf9442535daf692b60f5df09ed2ce315ee
Michael du Plessis <mdp0101@gmail.com>
2022-11-11 16:46:26 +0200

    code

--- src/level2.rs
+++ src/level2.rs
@@ -1,8 +1,25 @@
 use crate::level1::count_coins;
 
-type Coords = (usize, usize);
+#[derive(Clone, Copy)]
+pub struct Coords(usize, usize);
 
-struct Game {
+impl std::ops::Sub for Coords {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0 - other.0, self.1 - other.1)
+    }
+}
+
+impl std::ops::Add for Coords {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+pub struct Game {
     size: usize,
     board: Vec<Vec<char>>,
     movements: String,
@@ -25,6 +42,26 @@ pub fn count_coins_over(file: &str) {
 
         array.push(new_row);
     }
+}
 
+impl Game {
+    fn move_pac(&mut self) {
+        let mut count = 0;
 
+        for m in self.movements.chars() {
+            let new_pos: Coords = match m {
+                'D' => self.pacman + (0, -1),
+                'U' => self.pacman + (0, 1),
+                'L' => self.pacman + (-1, 0),
+                'R' => self.pacman + (0, 1),
+                _ => 0
+            };
+
+            if self.board[new_pos.0][new_pos.1] == 'C' {
+                count += 1;
+            }
+        }
+
+        std::fs::write("./out", count.to_string())
+    }
 }
