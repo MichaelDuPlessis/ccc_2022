fd7f636199ec032fc2ba7f95d0996625b686a175
Michael du Plessis <mdp0101@gmail.com>
2022-11-11 16:27:21 +0200

    board

--- src/level2.rs
+++ src/level2.rs
@@ -1,7 +1,22 @@
 use crate::level1::count_coins;
 
+type Coords = (usize, usize);
+
+struct Game {
+    size: usize,
+    board: Vec<Vec<char>>,
+    movements: String,
+    pacman: Coords,
+}
+
 pub fn count_coins_over(file: &str) {
     let input = std::fs::read_to_string(format!("./level2/{}.in", file)).unwrap();
 
     let lines = input.lines();
 }
+
+
+
+
+
+
