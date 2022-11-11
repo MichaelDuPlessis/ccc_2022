a4dde63fb30e68ffaa605d424a8bda938287d92f
Michael du Plessis <mdp0101@gmail.com>
2022-11-11 17:47:29 +0200

    code

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,9 @@ mod level2;
 pub mod level3;
 
 fn main() {
-    let game = Game::new("level2_1");
-
-    println!("{:?}", game);
-
     for i in 1..6 {
         level3::Game::new(&format!("level3_{}", i)).move_pac();
     }
 
-    level2::Game::new(&format!("level2_{}", "example")).move_pac();
+    level2::Game::new(&format!("level3_{}", "example")).move_pac();
 }
