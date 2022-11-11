84888d5ba5b10cb80a0953cc89a429969a7222e3
Michael du Plessis <mdp0101@gmail.com>
2022-11-11 18:49:46 +0200

    code

--- src/level4.rs
+++ src/level4.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 use petgraph::{prelude::UnGraph, graph};
 
 const LEVEL: &str = "level4";
@@ -23,9 +25,40 @@ impl Game {
         let mut lines = input.lines();
         let size = lines.next().unwrap().parse::<usize>().unwrap();
 
-        let mut input = lines.collect::<String>();
-        println!("{input}");
-        input.retain(|c| !c.is_whitespace());
+        for (i, line) in lines.enumerate() {
+            for (j, c) in line.chars().enumerate() {
+                 match c {
+                    'P' => {
+                        graph.add_node(Entity::Pacman);
+                    },
+                    'C' => {
+                        graph.add_node(Entity::Coin);
+                    },
+                    'G' => {
+                        graph.add_node(Entity::Ghost);
+                    },
+                    'W' => {
+                        graph.add_node(Entity::Wall);
+                    },
+                    _ => panic!("Why cruel world"),
+                };
+            }
+
+            if i == size - 1 {
+                break;
+            }
+        }
+
+        // adding edges
+        for i in 0..size*size {
+            if i != 0 {
+
+            }
+            
+            if i == size - 1 {
+                
+            }
+        }
 
         Self {
             graph
